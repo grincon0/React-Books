f5cc31b37b921adb652c0c14a097e75284805a4f
Wake <georgerincon2@gmail.com>
2019-01-22 00:29:22 -0500

    added SearchForm comp

--- client/src/components/Btns/index.js
+++ client/src/components/Btns/index.js
@@ -3,7 +3,9 @@ import "./style.css";
 
 function Btn(props) {
     return (
-        <button {...props} style={{ float: "right", marginBottom: 10 }} className="btn" />
+        <button {...props} style={{ float: "right", marginBottom: 10 }} className="btn">
+        {props.children}
+        </button>
     );
 
 }
--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -21,8 +21,8 @@ function Card(props) {
             
                 <h5 className="card-title">Special title treatment</h5>
                 <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <Btn className="btn-success" />
-                <Btn className="btn-danger" />
+                <Btn className="btn-success">View</Btn>
+                <Btn className="btn-danger">Save</Btn>
             </div>
         </div>
 
