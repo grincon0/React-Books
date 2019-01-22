264f5ee342c9eeb171e5074a7c6e9e2d04e0a1e0
Wake <georgerincon2@gmail.com>
2019-01-21 21:36:23 -0500

    dev rendering success

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
-const router = require("express").Router();
+
 const path = require("path");
-const apiRoutes = require("./api");
+const router = require("express").Router();
+
+//const apiRoutes = require("./api");
 
 
 //For API routes
 
-router.use("/api", apiRoutes);
-
-router.use(path.join(__dirname, "../client/public/index.html"));
+//router.use("/api", apiRoutes);
+router.use((req, res) => {
+    res.sendFile(path.join(__dirname, "../client/public/index.html"));
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
 const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static("client/build"));
 }
+
+
 app.use(routes);
+
 
 mongoose.connect(
     process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
